ec4dd59c3a30feff814184fc02c8723fa6a88ae8
syahrulzein <syahruls.zein@gmail.com>
2025-12-13 15:51:19 +0700

    init

---
+++ kontak.py
@@ -0,0 +1,8 @@
+import streamlit as st
+def contact_me():
+    st.write('**Contact Me**')
+    st.write('Jika Anda memiliki pertanyaan atau ingin berdiskusi lebih lanjut, silakan hubungi saya melalui email atau media sosial berikut:')
+    st.write('📧 Email: syahruls.zein@gmail.com ')
+    st.write('🔗 LinkedIn: [linkedin.com/in/syahrul-septian-zein](https://www.linkedin.com/in/syahrul-septian-zein/)')
+    st.write('🐱 GitHub: [github.com/syahrulzein](https://github.com/syahrulzein)')
+    st.write('Saya terbuka untuk kolaborasi dan diskusi seputar data science dan machine learning. Terima kasih telah mengunjungi aplikasi ini!')
